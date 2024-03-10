0dfab579eaf350638871d2a4d78ea55ab952c26c
bintang <bravo.t.msee@gmail.com>
2024-03-10 14:55:42 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 
 int main(int argc, char** argv) 
 {
+  if (argc == 1) {
+    printf("Input file missing, please provide a circuit netlist\n");
+    return 1;
+  }
   Tran::NetlistParser parser(argv[1]);
   Tran::Circuit circuit(parser);
   Tran::Simulator simulator(circuit);

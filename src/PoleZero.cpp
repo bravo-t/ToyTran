185ec0dcbc84fd41598891f597c590c0297daa2a
13inback <13inback@gmail.com>
2025-04-27 10:07:44 +0000

    moments are now correct

--- src/PoleZero.cpp
+++ src/PoleZero.cpp
@@ -70,12 +70,6 @@ PoleZeroAnalysis::calcMoments(const MatrixXd& G,
     Vprev = V;
   }
 
-  Eigen::MatrixXd A = C + G;
-  Eigen::EigenSolver<MatrixXd> es(A);
-  const Eigen::VectorXcd& ev = es.eigenvalues();
-  printf("DEBUG: Eigenvalue of A=C+G:\n");
-  for (long int i=0; i<ev.rows(); ++i) printf("%.6f+%.6fi ", ev(i).real(), ev(i).imag());
-  printf("\n");
   return true;
 }
     
@@ -240,7 +234,7 @@ PoleZeroAnalysis::run()
   calcResidues(_poles, outputMoments, _residues);
   _moments.assign(outputMoments.begin(), outputMoments.end());
   printf("Moments:\n");
-  for (double m : _moments) printf("%.6f ", m);
+  for (double m : _moments) printf("%.6G ", m);
   printf("\n");
   
   printf("Poles:\n");
